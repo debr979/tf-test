7bae8fda92ef7a3fcb8e2b22eb10388eab714897
Doug <debr979@gmail.com>
2020-02-26 16:40:45 +0800

    first push

---
+++ go.mod
@@ -0,0 +1,5 @@
+module tf-test
+
+go 1.13
+
+require github.com/tensorflow/tensorflow v2.1.0+incompatible // indirect
---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	tf "github.com/tensorflow/tensorflow/tensorflow/go"
+	"github.com/tensorflow/tensorflow/tensorflow/go/op"
+	"log"
+)
+
+func main() {
+
+	s := op.NewScope()
+	c := op.Const(s, "Hello from TensorFlow version "+tf.Version())
+	graph, err := s.Finalize()
+	if err != nil {
+		log.Println(err)
+	}
+	sess, err := tf.NewSession(graph, nil)
+	if err != nil {
+		log.Println(err)
+	}
+	output, err := sess.Run(nil, []tf.Output{c}, nil)
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println(output[0].Value())
+}
